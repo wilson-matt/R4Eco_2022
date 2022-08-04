b387e5f85c6e908249b16c2aad9e879800fb7118
Matt Wilson <77399165+wilson-matt@users.noreply.github.com>
2022-08-04 16:07:03 -0400

    Update smart_wd.R

--- Functions/smart_wd.R
+++ Functions/smart_wd.R
@@ -1,3 +1,17 @@
-if (Sys.info()[["sysname"]]=="Windows") {setwd("C:/GitHub")} else {
-  if (Sys.info()[["sysname"]]=="Darwin"){setwd("~/GitHub")} else print("for Linux run: setwd('/home/[INSERT YOUR USER NAME]/GitHub')")
+  smart_wd <- function (repo, folder=NULL) {
+    if(missing(folder))
+    {
+      setwd(print(paste(if (Sys.info()[["sysname"]]=="Windows") {("C:/GitHub")} else {
+        if (Sys.info()[["sysname"]]=="Darwin"){("~/GitHub")} else ("for Linux run: setwd('/home/[INSERT YOUR USERNAME]/GitHub')")
+      },"/",repo, sep = "")))}
+    else 
+     
+    {
+      setwd(print(paste(if (Sys.info()[["sysname"]]=="Windows") {("C:/GitHub")} else {
+        if (Sys.info()[["sysname"]]=="Darwin"){("~/GitHub")} else ("for Linux run: setwd('/home/[INSERT YOUR USERNAME]/GitHub')")
+      },"/",repo, "/", folder, sep = "")))}
   }
+
+
+smart_wd(repo = "R4Eco_2022", folder = "Startu")
+
