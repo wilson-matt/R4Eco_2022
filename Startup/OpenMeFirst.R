b79634a07c930380d3601f6a5af347fc49992a2b
Matt Wilson <77399165+wilson-matt@users.noreply.github.com>
2022-08-04 13:26:34 -0400

    Create OpenMeFirst.R

---
+++ Startup/OpenMeFirst.R
@@ -0,0 +1,3 @@
+#set working directory to the R for Ecology repository on GitHub
+source('https://raw.githubusercontent.com/SU-RProjects/R4Eco_2022/main/Functions/smart_wd.R?token=GHSAT0AAAAAABXKCRVALP7OT5P6DMCIEQQSYXMAD6A')
+
