5d445bf3ca14d78e25dc635381d602461956270c
Matt Wilson <77399165+wilson-matt@users.noreply.github.com>
2022-08-04 13:23:46 -0400

    Functions folder added

--- Functions/smart_wd.R
+++ Functions/smart_wd.R
@@ -1,3 +1,3 @@
-if (Sys.info()[["sysname"]]=="Wndows") {setwd("C:/GitHub/R4Eco_2022")} else {
+if (Sys.info()[["sysname"]]=="Windows") {setwd("C:/GitHub/R4Eco_2022")} else {
   if (Sys.info()[["sysname"]]=="Darwin"){setwd("~/GitHub/R4Eco_2022")} else print("for Linux run: setwd('/home/[INSERT YOUR USER NAME]/GitHub/R4Eco_2022')")
   }
