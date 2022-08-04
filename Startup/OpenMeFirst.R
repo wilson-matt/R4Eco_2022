a6650fe64c150a703e7ff8ec1428356f01c38f85
Matt Wilson <77399165+wilson-matt@users.noreply.github.com>
2022-08-04 13:38:42 -0400

    Update OpenMeFirst.R

--- Startup/OpenMeFirst.R
+++ Startup/OpenMeFirst.R
@@ -1,4 +1,4 @@
 #set working directory to the R for Ecology repository on GitHub
 source('https://raw.githubusercontent.com/SU-RProjects/R4Eco_2022/main/Functions/smart_wd.R?token=GHSAT0AAAAAABXKCRVALP7OT5P6DMCIEQQSYXMAD6A')
 
-
+#to change the working directory, we will open the "Functions" folder within your own repository (NOT the course repository) and change "R4Eco_2022" to your repo name (last name, first initial, middle initial).
