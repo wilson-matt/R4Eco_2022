a6b548a3d00aafe6a8fabab08e0fefec0ca13595
Matt Wilson <77399165+wilson-matt@users.noreply.github.com>
2022-08-24 15:20:15 -0400

    Create Week1_Assignment.R

---
+++ Week1/Week1_Assignment.R
@@ -0,0 +1,25 @@
+# Now it is time to create your own data frame using the tools we have learned this week.
+# First, resave this script as: your last name_Week1_Assignment
+  # e.g. mine would be Wilson_Week1_Assignemnt
+
+
+# Create 3 numeric vectors and 2 character vectors that are each 15 values in length with the following structures:
+  # One character vector with all unique values
+  # One character vector with exactly 3 unique values
+  # One numeric vector with all unique values
+  # One numeric vector with some repeated values (number of your choosing)
+  # One numeric vector with some decimal values (of your choosing)
+
+# Bind the vectors into a single data frame, rename the columns, and make the character vector with unique values the row names.
+
+# Remove the character vector with unique values from the data frame.
+
+# Add 1 row with unique numeric values to the data frame.
+
+# Export the data frame as a .csv file 
+
+# Generate summary statistics of your data frame and copy them as text into your script under a new section heading.
+
+# Push your script and your .csv file to GitHub in a new "Week1" folder.
+
+
